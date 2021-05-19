36a9d700c6f8b71a7f49956aceb5080f7e2f796b
bearinawolfpack <bearinawolfpack@gmail.com>
2021-05-19 16:29:13 -0400

    Add boilerplate code for ColorChanger

---
+++ main.js
@@ -0,0 +1,6 @@
+let title = document.querySelector("#title");
+let colorChanger = document.querySelector("#colorChanger");
+
+colorChanger.addEventListener("click", () => {
+  console.log("I've been clicked!")
+})
