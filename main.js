8f26387d8116c9515c89dc9fa1352f7232300587
bearinawolfpack <bearinawolfpack@gmail.com>
2021-05-19 20:30:46 -0400

    Add feature colorControl to ColorChanger
    colorControl ensures that title will be visible
    to the user regardless of the body background-color

--- main.js
+++ main.js
@@ -3,14 +3,29 @@ let colorChanger = document.querySelector('#colorChanger');
 let body = document.querySelector('body');
 
 colorChanger.addEventListener('click', () => {
-  let color = colorRandomize();
-  title.innerHTML = `${color}`;
-  body.style = `background-color: ${color}`;
+  let colors = colorControl(colorRandomize());
+  console.log(colors);
+  title.innerHTML = `${colors[1]}`;
+  title.style = `color: ${colors[0]}`;
+  body.style = `background-color: ${colors[1]}`;
 });
 
 const colorRandomize = () => {
   let r = Math.floor(Math.random() * 256);
   let g = Math.floor(Math.random() * 256);
   let b = Math.floor(Math.random() * 256);
-  return `rgb(${r},${g},${b})`
-}
+  let rgb = [r, g, b];
+  return rgb;
+};
+
+const colorControl = (rgbArr) => {
+  let r1 = rgbArr[0];
+  let g1 = rgbArr[1];
+  let b1 = rgbArr[2];
+  let r2 = Math.abs(r1 - 255);
+  let g2 = Math.abs(g1 - 255);
+  let b2 = Math.abs(b1 - 255);
+  let backgroundColor = `rgb(${r1}, ${g1}, ${b1})`;
+  let textColor = `rgb(${r2}, ${g2}, ${b2})`;
+  return [backgroundColor, textColor];
+};
