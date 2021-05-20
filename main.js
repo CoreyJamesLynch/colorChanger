dcdc1f5a1a1f59a0a7788875059c29d3ed6a01b4
bearinawolfpack <bearinawolfpack@gmail.com>
2021-05-19 20:07:46 -0400

    Add colorRadomize and button event to ColorChanger

--- main.js
+++ main.js
@@ -1,6 +1,16 @@
-let title = document.querySelector("#title");
-let colorChanger = document.querySelector("#colorChanger");
+let title = document.querySelector('#title');
+let colorChanger = document.querySelector('#colorChanger');
+let body = document.querySelector('body');
 
-colorChanger.addEventListener("click", () => {
-  console.log("I've been clicked!")
-})
+colorChanger.addEventListener('click', () => {
+  let color = colorRandomize();
+  title.innerHTML = `${color}`;
+  body.style = `background-color: ${color}`;
+});
+
+const colorRandomize = () => {
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
+  let b = Math.floor(Math.random() * 256);
+  return `rgb(${r},${g},${b})`
+}
